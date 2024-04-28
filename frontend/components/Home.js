b9ebaea3a8db832f09ef298c4a2f3127021f2668
Ali <idris1390@gmail.com>
2024-04-29 01:10:26 +0200

    test

---
+++ frontend/components/Home.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css';
+
+function Home() {
+  return (
+    <div>
+      <main className={styles.main}>
+        
+      </main>
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/components/Menu.js
@@ -0,0 +1,27 @@
+import styles from '../styles/Menu.module.css';
+
+import Link from 'next/link';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Menu() {
+  return (
+
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home" className={styles.main}></Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+        </Navbar>
+    )}
+
+    
+export default Menu;
