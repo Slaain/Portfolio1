c3a9668408b6e09b5eeff1890dd285bb4431bc18
Ali <idris1390@gmail.com>
2024-05-14 17:50:59 +0200

    ok

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -4,7 +4,11 @@ function Home() {
   return (
     <div>
       <main className={styles.main}>
-        
+        <div>Idris ALI</div>
+        <div>Je suis développeur web FULLSTACK</div>
+        <div>   en apprentissage </div>
+         <i className={styles.arrow}></i>
+         <i className={styles.arrow2}></i>
       </main>
     </div>
   );
--- frontend/components/Menu.js
+++ frontend/components/Menu.js
@@ -1,28 +1,17 @@
 import styles from '../styles/Menu.module.css';
-
 import Link from 'next/link';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 
 function Menu() {
   return (
-
-    <Navbar className={`${styles.main} custom-navbar`}>
-        <Container className={styles.men} >
-          <div  className={styles.man} style={{ color: 'whitesmoke' }}>
-          <Navbar.Brand href="#home" ></Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Accueil</Nav.Link>
-            <Nav.Link href="#features">CV</Nav.Link>
-            <Nav.Link href="#features">SKILLS</Nav.Link>
-            <Nav.Link href="#features">A PROPOS</Nav.Link>
-            <Nav.Link href="#pricing">CONTACT</Nav.Link>
-          </Nav>
-          </div>
-        </Container>
-        </Navbar>
+      <div className={styles.menueContainer}>
+          <Link href="/"><div className={styles.link}>Accueil</div></Link>
+          <Link href="/"><div className={styles.link}>A propos</div></Link>
+          <Link href="/"><div className={styles.link}>CV</div></Link>
+          <Link href="/"><div className={styles.link}>Skills</div></Link>
+          <Link href="/"><div className={styles.link}>Portfolio</div></Link>
+          <Link href="/"><div className={styles.link}>Contact</div></Link>
+      </div>
+   
     )}
 
     
