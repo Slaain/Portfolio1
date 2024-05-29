b6b4155c15277d3e0cceadef7c9c0d0d1bd8e49b
Ali <idris1390@gmail.com>
2024-05-29 20:39:10 +0200

    page a propos fini

--- frontend/components/About.js
+++ frontend/components/About.js
@@ -1,23 +1,60 @@
+import { faGreaterThan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from '../styles/About.module.css';
-import Link from 'next/link';
 import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+import Infos from './infos';
 
-function About() {
+function About(props) {
+  const initialData = [
+    { 
+      nom: 'Idris ALI', 
+      tel: '07.67.43.05.75', 
+      adresse: '13 rue de l\'amélie', 
+      pays: 'France', 
+      age: '26', 
+      etude: 'License de niveau 6', 
+      email: 'idris1390@gmail.com', 
+      status: 'En recherche d\'alternance' 
+    }
+  ];
+
+  const [infoData, setInfoData] = useState(initialData);
+
+  const infos = infoData.map((data, i) => (
+    <Infos
+      key={i}
+      
+      icon={<FontAwesomeIcon icon={faGreaterThan} />}  nom={data.nom}
+      tel={data.tel}
+      adresse={data.adresse}
+      pays={data.pays}
+      age={data.age}
+      etude={data.etude}
+      email={data.email}
+      status={data.status}
+    />
+  ));
+
   return (
-      <div className={styles.propocontainer}>
-           <div className={styles.txt}>A Propos</div>
-           <div className={styles.txt2}>A Propos</div>
+    <div className={styles.propocontainer}>
+      <div className={styles.txt}>A Propos</div>
+      <div className={styles.txt2}>A Propos</div>
       <div className={styles.moiImg}>
-           <Image src="/moiok.jpg" alt="Moi" width={250} height={290}/>
+        <Image src="/moiok.jpg" alt="Moi" width={250} height={290} />
       </div>
       <div className={styles.textDesc}>
-          <p>Olivier Prieur Je m'appelle Olivier Prieur. Je suis développeur web à Nevers, en région Bourgogne ouest. J’ai repris les cours en 2020 afin d’acquérir de nouvelles compétences en développement web qui complètent aujourd’hui celles déjà acquises dans le cadre des mes précédentes missions et dans le cadre de projets associatifs. Je suis aujourd’hui titulaire d’un Titre Professionnel BAC+2 « Développeur web et web mobile ». Je code principalement en PHP et j'ai adopté le framework Symfony comme mon outil de travail principal.
-J'ai aussi pu acquérir de nouvelles compétences liées aux bonnes pratiques et aux savoir-faire professionnels indispensables dans le métier de développeur web (gestion de projets, versioning, ...).
-Je possède également des compétences d'administration réseau, d'admininistration système Linux et quelques notions DevOps (Docker, Gitlab CI, ...). Libriste et linuxien, j'aime les temps conviviaux des install parties. Je blog (www.citizenz.info). J'ai créé une cryptomonnaie début 2020 : PengolinCoin (www.pengolincoin.xyz). </p>
+        <p>
+          Je m'appelle Idris ALI. Je suis développeur web en recherche d'une alternance dans le département des Bouches-du-Rhône. Actuellement en formation Concepteur Développeur d'Applications, je suis titulaire d’un Titre Professionnel BAC+2 « Développeur Web et Web Mobile ». Je code principalement en PHP et j'ai adopté le framework Symfony comme mon outil de travail principal.
+          Grâce à ma formation de Concepteur Développeur d'Applications, j'ai pu acquérir de nouvelles compétences, notamment la maîtrise du framework React, React Native, Node.js et TypeScript, ainsi que la base de données MongoDB. Cette formation m'a aidé à approfondir mes compétences et à apprendre de bonnes pratiques, telles que l'utilisation des reducers, pour développer des applications sécurisées et fonctionnelles.
+          Pour continuer mon parcours dans ce domaine qui me passionne tant, je souhaite poursuivre mes études à l'école EPITECH et obtenir un Master en Architecture Logicielle. Cette opportunité représente un objectif crucial pour moi, me permettant d'acquérir les connaissances nécessaires pour réussir dans ce métier.
+        </p>
       </div>
+      <div className={styles.borderz}>
+        {infos}
       </div>
-   
-    )}
+    </div>
+  );
+}
 
-    
-export default About
+export default About;
--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.css';
 import About from '../components/About';
+import Infos from './infos';
 import Accueil from '../components/Accueil';
 import Menu from '../components/Menu';
+import { faGreaterThan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function Home() {
+ 
+  // console.log("voici la :", infoData);
+
   return (
     <div className={styles.mains}>
       <div className={styles.bg}>
-         <Menu />
-      <Accueil />
+        <Menu />
+        <Accueil />
       </div>
-     
       <About />
+     
+      {/* <Card /> */}
     </div>
   );
 }
