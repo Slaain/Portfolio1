6dbd84b52d03eb470f15056570c3e707c868923a
Ali <idris1390@gmail.com>
2024-07-07 19:38:27 +0200

    respons

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ function App({ Component, pageProps }) {
     <>
       <Head>
         <title>IDRIS ALI</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
     </>
